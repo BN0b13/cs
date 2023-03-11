e4f9f2b025309972ef8b342a7b1f659bf9f2c2ab
BN0b13 <NobleNotes1@gmail.com>
2023-03-11 14:14:37 -0800

    bash tinkering

--- start.sh
+++ start.sh
@@ -4,13 +4,5 @@
 git pull origin main;
 # Build React app with said copy
 npm run build;
-# Navigate to backend dir
-cd ~/server/cs-backend
-# Stop PM2
-pm2 stop 4
-# Pull new changes to backend main
-git pull origin main
-# Restart PM2
-pm2 start 4
 # Restart Nginx to reflect new changes 
 systemctl restart nginx;
