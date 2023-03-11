6a10a133f1f69dd4b43cf5d991891ce0a7b6782d
BN0b13 <NobleNotes1@gmail.com>
2023-03-11 13:33:58 -0800

    superficial change to test bash script

--- src/pages/home/home.pages.jsx
+++ src/pages/home/home.pages.jsx
@@ -4,9 +4,6 @@ import Welcome from '../../components/welcome/welcome.component';
 import './home.styles.scss';
 
 const HomePage = ({ currentHomeDisplay }) => {
-
-    console.log('Home Page currentHomeDisplay var: ', currentHomeDisplay);
-    
     return (
         <Welcome />
     );
