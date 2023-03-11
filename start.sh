941f8a366af4d7c37b56837d748c4f38f33c1a2c
BN0b13 <NobleNotes1@gmail.com>
2023-03-11 13:44:55 -0800

    added 2nd bash script

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Pull latest copy of main
+# Pull new changes to frontend main
 git pull origin main;
 # Build React app with said copy
 npm run build;
+# Navigate to backend dir
+cd ~/server/cs-backend
+# Stop PM2
+pm2 stop 4
+# Pull new changes to backend main
+git pull origin main
+# Restart PM2
+pm2 start 4
 # Restart Nginx to reflect new changes 
 systemctl restart nginx;
---
+++ sudo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run start-up script as sudo
+sudo ./start.sh
