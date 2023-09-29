cf5361e8052a77264f3de41c9773998a6ab3a4d0
BN0b13 <NobleNotes1@gmail.com>
2023-09-29 09:17:16 -0700

    Punched up some language

--- src/pages/about/about.pages.jsx
+++ src/pages/about/about.pages.jsx
@@ -26,29 +26,32 @@ const AboutPage = () => {
                 <AboutPageImage src={logo} />
                 <AboutPageTitle>About Cosmic Strains</AboutPageTitle>
                 <ContentTextContainer topContainer={true}>
-                    <AboutPageText><AboutPageTextBold>OUR MISSION</AboutPageTextBold> at Cosmic Strains is to create the most stable and potent genetics around. This journey will take time and result in a large number of F1 cannabis strains. More than we could ever get to which means our customers are able to collect a piece of our history with never before grown strains. All mother plants have been thoroughly grown and tested for yield, structure, potency, resin production and aroma. Our goal is to eventually make our own F4 cannabis strains and we know how important it is to start with a wide pool of strains.</AboutPageText>
+                    <AboutPageText><AboutPageTextBold>OUR MISSION</AboutPageTextBold> at Cosmic Strains transcends mere cultivation—it's about crafting the most stable, potent cannabis genetics imaginable. This cosmic journey is an ongoing odyssey, resulting in an ever-expanding constellation of F1 cannabis strains. More than just strains, they are a piece of our living history, a testament to our relentless pursuit of perfection. Our journey begins with mother plants subjected to rigorous tests for yield, structure, potency, resin production, and aroma, ensuring only the finest genetics make it to your collection.</AboutPageText>
                 </ContentTextContainer>
                 <ContentTextContainer>
-                    <AboutPageText><AboutPageTextBold>OUR PROMISE</AboutPageTextBold> is to always offer our collectible full pack F1 seeds at $64.99. Our full pack collectibles contain 12 regular seeds from our lab, where we work night and day to bring together the best genetics.</AboutPageText>
+                    <AboutPageText><AboutPageTextBold>OUR PROMISE</AboutPageTextBold> is unwavering: you'll always find our coveted full pack F1 seeds priced at just $64.99. Each pack contains 12 regular seeds, born from our tireless efforts to unite the very best genetics under one celestial roof.</AboutPageText>
                 </ContentTextContainer>
                 <ContentTextContainer>
-                    <AboutPageText><AboutPageTextBold>OUR GENETICS</AboutPageTextBold> are a mixture of some of the rarest and most popular strains around. While we completely love the new strains other breeders are coming out with, we also will always hold an affinity in our hearts for where we came from. That means we sought out seeds from legitimate, old school strains such as Sour Diesel, SFVOG, Train Wreck, Purple Haze, Super Silver Haze, Super Lemon Haze, OG Kush, or containing Skunk, Big Bud, Black Widow (known as White Widow in the 90’s) and many more. And as for where we are now? We have also painstakingly pursued getting some of the rarest modern strains such as Pineapple Express, Bruce Banner and Ghost Train Haze, Sugar Cane, Blueberry Muffin or containing Ghost OG, Wedding Cake, Dosidos, Stardawg, Face Off OG and many more. Over the last few years, we have worked hard to refine the strains that we offer. If it doesn’t live up to our standards, it gets chopped down, no exceptions.</AboutPageText>
+                    <AboutPageText><AboutPageTextBold>OUR GENETICS</AboutPageTextBold> consist of an intricate tapestry of rare classics and contemporary legends, an homage to our roots and an embrace of the future. From cherished old-school strains like Sour Diesel and OG Kush to the rarest modern marvels like Sugar Cane and Blueberry Muffin, we've curated a selection that reflects the essence of cannabis culture.</AboutPageText>
                     <AboutPageTextBold>- Quality is everything -</AboutPageTextBold>
                 </ContentTextContainer>
                 <ContentTextContainer>
-                    <AboutPageText><AboutPageTextBold>OUR PLAN</AboutPageTextBold> is to approach this launching of F4 strains meticulously and carefully. We will be working with fruity, gassy, chemy, og, sours, cookies, diesels, hazes…basically any category of terpene that gets people moving. This will take time and so we have planned out our phases to represent different milestones reached during this trek.</AboutPageText>
+                    <AboutPageText><AboutPageTextBold>OUR PLAN</AboutPageTextBold> to create and curate the universes greatest strains will require time and dedication to supreme quality. If a strain doesn't meet our exacting standards, it simply doesn't make the cut—no exceptions. Quality is not just a goal; it's our guiding star.</AboutPageText>
                 </ContentTextContainer>
                 <ContentTextContainer>
                     <AboutPageTextBold>PHASE 0</AboutPageTextBold>
-                    <AboutPageText>Phase 0 will last until some time in 2025. During which we will be launching a number of different and interesting lines. Each line will represent one of the terpene or plant characteristics we are after.</AboutPageText>
+                    <AboutPageText>Launching a constellation of diverse and intriguing lines, each representing a unique terpene or plant characteristic. Phase 0 extends until 2025, setting the stage for what lies beyond.</AboutPageText>
                 </ContentTextContainer>
                 <ContentTextContainer>
                     <AboutPageTextBold>PHASE 1</AboutPageTextBold>
-                    <AboutPageText>Phase 1 will start with a pause in new lines as we gear up to start working on what we have created thus far. There will most likely be shifting in the mother strains that have been previously available as we make room to let our own strains flourish. Once we are satisfied with what we have chosen, Phase 1 will take on a similar run to the previous phase where new lines will become available.</AboutPageText>
+                    <AboutPageText>A moment of reflection and refinement. We pause the creation of new lines to nurture the strains we've birthed. Expect shifts in our mother strains, allowing room for our unique creations to flourish. Once ready, Phase 1 ushers in fresh lines of innovation.</AboutPageText>
                 </ContentTextContainer>
                 <ContentTextContainer>
                     <AboutPageTextBold>PHASE 2</AboutPageTextBold>
-                    <AboutPageText>Phase 2 will be the longest phase, but will come with the most rewards. During this phase we will begin the exciting journey of creating our own stable F4 lines. Once the F4 lines have been created, Cosmic Strains will offer these collectible seeds on a consistent basis.</AboutPageText>
+                    <AboutPageText>The zenith of our journey, where patience bears the sweetest fruit. In Phase 2, we embark on the thrilling quest to craft our very own stable F4 lines. These lines represent the culmination of our cosmic dedication, and Cosmic Strains will offer them consistently, elevating your collection to the stars.</AboutPageText>
+                </ContentTextContainer>
+                <ContentTextContainer>
+                    <AboutPageText><AboutPageTextBold>JOIN US</AboutPageTextBold> in this cosmic cultivation odyssey, where genetics meet the infinite. At Cosmic Strains, we're not just collectors; we're cosmic curators, dedicated to nurturing the cannabis genetics of tomorrow. Welcome to the cosmos of Cannabis, where the stars are the limit.</AboutPageText>
                 </ContentTextContainer>
             </ContentContainer>
             <ButtonContainer>
