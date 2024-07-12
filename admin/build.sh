bb0d6ffe7ee125932f804a37a82e9dfdda048abc
BN0b13 <NobleNotes1@gmail.com>
2024-07-11 19:57:51 -0700

    adding to set up bash scripts

--- admin/build.sh
+++ admin/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Clear local git changes, if any
-git reset --hard
-# Pull new changes to frontend main
-git pull origin main;
 # Install any new npm packages
 npm install
 # Build React app with said copy
