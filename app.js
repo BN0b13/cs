b335bc3548eca927f5063e1200c02e3b41ac4f06
BN0b13 <NobleNotes1@gmail.com>
2024-08-22 06:01:20 -0700

    Removed mongoose reference

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 require('dotenv/config');
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-// mongoose.connect(process.env.CONNECTIONSTRING, { 
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-//   }).then(() => {
-//     console.log('DB Connected');
-//   }).catch(err=>{
-//     return console.log(err);
-// });
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(
